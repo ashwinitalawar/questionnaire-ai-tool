ee5ad2efaf2aad74f88b10e698b67022fdfc6701
Ashwini Talawar <atalawar289@gmail.com>
2026-03-08 10:57:23 +0530

    Fix SQLite threading issue

--- auth.py
+++ auth.py
@@ -4,33 +4,44 @@ from passlib.hash import bcrypt
 DB_NAME = "users.db"
 
 
-def get_connection():
-    return sqlite3.connect(DB_NAME, check_same_thread=False)
-
-
-def hash_password(password):
-    return bcrypt.hash(password)
-
-
-def register(email, password):
-
-    conn = get_connection()
+def create_table():
+    conn = sqlite3.connect(DB_NAME)
     cursor = conn.cursor()
 
-    hashed_password = hash_password(password)
-
-    cursor.execute(
-        "INSERT INTO users (email, password) VALUES (?, ?)",
-        (email, hashed_password)
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        email TEXT PRIMARY KEY,
+        password TEXT
     )
+    """)
 
     conn.commit()
     conn.close()
 
 
+def register(email, password):
+
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+
+    hashed_password = bcrypt.hash(password)
+
+    try:
+        cursor.execute(
+            "INSERT INTO users (email, password) VALUES (?, ?)",
+            (email, hashed_password)
+        )
+        conn.commit()
+
+    except sqlite3.IntegrityError:
+        pass
+
+    conn.close()
+
+
 def login(email, password):
 
-    conn = get_connection()
+    conn = sqlite3.connect(DB_NAME)
     cursor = conn.cursor()
 
     cursor.execute(
@@ -43,7 +54,9 @@ def login(email, password):
     conn.close()
 
     if result:
-        stored_password = result[0]
-        return bcrypt.verify(password, stored_password)
+        return bcrypt.verify(password, result[0])
 
     return False
+
+
+create_table()
